9ee18f2f13e997405b45de2d3cfee77edb159d28
Ramin Mammadzada <remki-design@hotmail.com>
2021-03-04 10:28:55 +0300

    Add Eslint and Stylelint config files. Update codebase to fix issues shown by linters.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
