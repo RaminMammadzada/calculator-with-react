bfd1fdeac1a5057736ee42c6b930a30ee31c7a5d
Ramin Mammadzada <remki-design@hotmail.com>
2021-03-08 23:32:52 +0300

    Convert ES5 functions to ES6 arrow function.

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,13 @@ import './App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-function calculate(a, b) {
-  return a + b;
-}
+const calculate = (a, b) => a + b;
 
-function App() {
-  return (
-    <>
-      <Display result={calculate(5, 3).toString()} />
-      <ButtonPanel />
-    </>
-  );
-}
+const App = () => (
+  <>
+    <Display result={calculate(5, 3).toString()} />
+    <ButtonPanel />
+  </>
+);
 
 export default App;
