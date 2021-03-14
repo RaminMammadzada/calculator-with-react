9ab6804c4309dc29a7e9df5f85729b1cd037de50
Ramin Mammadzada <remki-design@hotmail.com>
2021-03-14 20:01:51 +0300

    Convert class components to functional components, hooks.

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
+const App = () => {
+  const [state, setState] = useState([{
+    total: null,
+    next: null,
+    operation: null,
+  }]);
 
-    this.handleClick = buttonName => {
-      const { total, next, operation } = calculate(this.state, buttonName);
-      this.setState({
+  const handleClick = useCallback(
+    buttonName => {
+      const { total, next, operation } = calculate(state, buttonName);
+      setState({
         total,
         next,
         operation,
       });
-    };
-  }
+    }, [state],
+  );
 
-  render() {
-    const { total, next } = this.state;
-
-    return (
-      <>
-        <div className="calculator">
-          <h1>Calculator with React</h1>
-          <Display result={next || total} />
-          <ButtonPanel clickHandler={buttonName => this.handleClick(buttonName)} />
-        </div>
-      </>
-    );
-  }
-}
+  const { total, next } = state;
+  return (
+    <>
+      <div className="calculator">
+        <h1>Calculator with React</h1>
+        <Display result={next || total} />
+        <ButtonPanel clickHandler={buttonName => handleClick(buttonName)} />
+      </div>
+    </>
+  );
+};
 
 export default App;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,50 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-export default class ButtonPanel extends Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-    this.state = {
-      buttonGroups: [
-        { buttons: ['AC', '+/-', '%', '/'] },
-        { buttons: ['7', '8', '9', 'x'] },
-        { buttons: ['4', '5', '6', '-'] },
-        { buttons: ['1', '2', '3', '+'] },
-        { buttons: ['0', '.', '='] },
-      ],
-    };
-  }
+const ButtonPanel = props => {
+  const initialState = {
+    buttonGroups: [
+      { buttons: ['AC', '+/-', '%', '/'] },
+      { buttons: ['7', '8', '9', 'x'] },
+      { buttons: ['4', '5', '6', '-'] },
+      { buttons: ['1', '2', '3', '+'] },
+      { buttons: ['0', '.', '='] },
+    ],
+  };
 
-  render() {
-    let buttonsView = [];
+  const [state] = useState(initialState);
 
-    const { buttonGroups } = this.state;
-    const { clickHandler } = this.props;
+  let buttonsView = [];
 
-    buttonsView = (
-      <div>
-        {buttonGroups.map(group => (
-          <div className="key-list" key={`${group.buttons.join('-')}`}>
-            {group.buttons.map(button => (
-              <Button
-                key={`btn-${button}`}
-                buttonName={button}
-                handleClick={button => clickHandler(button)}
-              />
-            ))}
-          </div>
-        ))}
-      </div>
-    );
-    return (
-      <div>
-        {buttonsView}
-      </div>
-    );
-  }
-}
+  const { buttonGroups } = state;
+  const { clickHandler } = props;
+
+  buttonsView = (
+    <div>
+      {buttonGroups.map(group => (
+        <div className="key-list" key={`${group.buttons.join('-')}`}>
+          {group.buttons.map(button => (
+            <Button
+              key={`btn-${button}`}
+              buttonName={button}
+              handleClick={button => clickHandler(button)}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+
+  return (
+    <div>
+      {buttonsView}
+    </div>
+  );
+};
 
 ButtonPanel.defaultProps = {
   clickHandler: null,
@@ -53,3 +50,5 @@ ButtonPanel.defaultProps = {
 ButtonPanel.propTypes = {
   clickHandler: PropTypes.func,
 };
+
+export default ButtonPanel;
